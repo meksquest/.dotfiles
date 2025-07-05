19a3f53f04bb31a6ccd2da0f14f2327af4f6e55b
meksquest <mmcclure0100@gmail.com>
2025-07-05 18:59:15 -0500

    feat: add keyboard repeat rate configuration to macOS defaults
    - Add InitialKeyRepeat and KeyRepeat settings for faster typing
    - Include documentation comments with reference URL
    - Remove console output messages for cleaner execution

--- scripts/macos-defaults.sh
+++ scripts/macos-defaults.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo "Updating macos config"
+# https://macos-defaults.com
+# you can see your settings via `defaults read com.apple.dock "autohide"`
+
 defaults write com.apple.dock "autohide" -bool "true"
 defaults write com.apple.dock "tilesize" -int "49"
 defaults write com.apple.dock "autohide-time-modifier" -float "0.2"
@@ -8,6 +10,7 @@ defaults write com.apple.dock "mineffect" -string "scale"
 defaults write com.apple.dock "static-only" -bool "true"
 # TODO: Not sure if this is working, came from amethyst docs
 defaults write com.apple.dock "workspaces-auto-swoosh" -bool "false"
+defaults write NSGlobalDomain "InitialKeyRepeat" -float "15"
+defaults write NSGlobalDomain "KeyRepeat" -float "2"
 
 killall Dock
-echo "Done updating macos config"
