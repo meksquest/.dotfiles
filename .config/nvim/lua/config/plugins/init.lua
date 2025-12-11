fb6fdf4492557f2b88421f7a51751f50056f9462
meksquest <mmcclure0100@gmail.com>
2025-12-11 15:41:30 +1300

    feat: remove inline git blame

--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -44,7 +44,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = {
-      current_line_blame = true,
+      current_line_blame = false,
     },
     init = function()
       vim.keymap.set('n', "[h", function()
